b01be1d06c633777f48b93a3cbf74ba4b399212b
nuclearBulldog <dellowcaleb@outlook.com>
2025-11-07 17:01:59 +1300

    dwm

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /*patches applied
  * - dwm-fixmultimon-6.4.diff
  * - dwm-focusmaster-return-6.2.diff
- *
+ * - dwm-colorbar-6.3.diff
  *
  * */
 
@@ -26,6 +26,11 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
