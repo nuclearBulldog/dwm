0aef0feaf7b1954fc6adb8b224059d555025d0c7
nuclearBulldog <dellowcaleb@outlook.com>
2025-11-07 16:49:07 +1300

    config

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /*patches applied
  * - dwm-fixmultimon-6.4.diff
- *
+ * - dwm-focusmaster-return-6.2.diff
  *
  *
  * */
@@ -15,8 +15,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=10", "Hack Nerd Font Mono:size=16", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true"  };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -92,10 +92,15 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	
+
+	/* multi-monitor Control*/
+	{ MODKEY,			XK_bracketright,       focusmon,       {.i = -1 } },
+	{ MODKEY|ShiftMask,		XK_bracketright,   tagmon,         {.i = -1 } },
+	{ MODKEY,			XK_bracketleft,        focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_bracketleft,    tagmon,         {.i = +1 } },
+
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
